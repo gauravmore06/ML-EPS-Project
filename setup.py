37207209da406c569e6b2b3d2d655df85c721096
Gaurav <gauravmore0206@gmail.com>
2025-08-05 13:50:17 +0530

    adding e

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() -> list[str]:
+
+    requirements_list = list[str] = []
+
+    return requirements_list
+
+
+
+setup(
+name= 'sensor',
+version= '0.0.1',
+author= 'Gaurav',
+author_email= 'gauravmore0206@gmai.com',
+packages= find_packages(),
+install_requires = get_requirements() #["pymongo"]
+
+
+
+)
